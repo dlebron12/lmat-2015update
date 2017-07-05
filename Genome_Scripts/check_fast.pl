33981c59ef4e70e5a9616376ea6af39180667a40
dlebron12 <dlebron@ucdavis.edu>
2017-07-05 14:28:48 -0700

    Add files via upload

--- Genome_Scripts/check_fast.pl
+++ Genome_Scripts/check_fast.pl
@@ -1,18 +1,19 @@
 #!/bin/perl
 
 #This script checks if the fastsummary for the genome was done and if not it resubmits the job.
+
 my $directory=$ARGV[0];
-my $catalog = $ARGV[1];
-open(my $sh,'<', $catalog)|| die "Couldnt open catalog";
+my $list="run.ls";
+open(my $sh,'<', $list)|| die "Couldn't open list";
 chdir $directory;
-while(my $catline=<$sh>){  
+while(my $seqid=<$sh>){  
    
-    chomp $catline;
-    my @info= split /\t/, $catline;
-    my $taxid=$info[3];
-   
-    my $seqid=$info[1];
-my $fast="$seqid/data.$seqid.fa.frag/$seqid.fa.frag.cleaned.db.lo.rl_output.0.30.fastsummary";
-if(! -f $fast){
- print "$seqid\n";chdir $seqid;system("/g/g19/bioinf/rs_dla.sh $seqid.fa.frag");chdir "../";}
+    chomp $seqid;
+    my $fast="$seqid/data.$seqid.fa.frag/$seqid.fa.frag.cleaned.db.lo.rl_output.0.30.fastsummary";
+    if(! -f $fast){
+    	print "$seqid\n";
+	chdir $seqid;
+	system("/g/g19/bioinf/rs_dla.sh $seqid.fa.frag");
+	chdir "../";
+	}
  } close $sh;
