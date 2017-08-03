e9078f2782ba9508247c6c28c7cde9518e2bec8c
dlebron12 <dlebron@ucdavis.edu>
2017-08-02 21:46:26 -0700

    Added method to download fasta file

--- get_ncbi_genomes.py
+++ get_ncbi_genomes.py
@@ -2,6 +2,14 @@
 import ftplib
 import os,sys
 
+
+###Method###
+def getFile(ftp, filename):
+    try:
+        ftp.retrbinary("RETR " + filename ,open(filename, 'wb').write)
+    except:
+        print "Error"
+
 ##### Input is list of accessions to download
 ##Please refer to confluence entrance:
 ##Example list of accessions can be found here:
@@ -12,11 +20,15 @@ file=sys.argv[0]
 link=
 user='anonymous'
 email='lebronaldea1@llnl.gov'
-ftp=ftplib.FTP(link,user,email)
+ftp=ftplib.FTP(link)
+ftp.login(user,email))
 #for each accession, divide the accession and create wd
 
 for accession in file_ls:
     cd=
     ftp.cwd(cd)
     fasta_file=
-    ftp.get()
+    #get fasta file
+    getFile(ftp,fasta_file)
+
+ftp.quit()
